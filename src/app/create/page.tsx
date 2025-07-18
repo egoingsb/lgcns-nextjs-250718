521f8514a7828461146d7f763cc32894b4101c17
egoing <egoing@gmail.com>
2025-07-18 11:35:06 +0900

    path가 /create 라면 /src/app/create/page.tsx를 사용한다.

---
+++ src/app/create/page.tsx
@@ -0,0 +1,30 @@
+export default function Create() {
+    return (
+        <div>
+            <h1>
+                <a href="/">Web</a>
+            </h1>
+            <ol>
+                <li>
+                    <a href="/read/1">html</a>
+                </li>
+                <li>
+                    <a href="/read/2">css</a>
+                </li>
+            </ol>
+            <h2>Create</h2>
+            Hello, Create
+            <ul>
+                <li>
+                    <a href="/create">create</a>
+                </li>
+                <li>
+                    <a href="/update/id">update</a>
+                </li>
+                <li>
+                    <button>delete</button>
+                </li>
+            </ul>
+        </div>
+    );
+}
