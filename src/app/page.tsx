f6b292a860a062c6a1d5e448ecdb4fabf6520db2
egoing <egoing@gmail.com>
2025-07-18 11:49:57 +0900

    layout.tsx에는 공통 레이아웃이 위치하고, 각 세그먼트에 대응하는 page.tsx에는 세그먼트에 해당하는 내용이 위치한다.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,8 @@
 export default function Home() {
     return (
         <div>
-            <h1>
-                <a href="/">Web</a>
-            </h1>
-            <ol>
-                <li>
-                    <a href="/read/1">html</a>
-                </li>
-                <li>
-                    <a href="/read/2">css</a>
-                </li>
-            </ol>
             <h2>Welcome</h2>
             Hello, WEB
-            <ul>
-                <li>
-                    <a href="/create">create</a>
-                </li>
-                <li>
-                    <a href="/update/id">update</a>
-                </li>
-                <li>
-                    <button>delete</button>
-                </li>
-            </ul>
         </div>
     );
 }
--- src/app/read/[id]/page.tsx
+++ src/app/read/[id]/page.tsx
@@ -6,30 +6,8 @@ export default async function Read({
     const { id } = await params;
     return (
         <div>
-            <h1>
-                <a href="/">Web</a>
-            </h1>
-            <ol>
-                <li>
-                    <a href="/read/1">html</a>
-                </li>
-                <li>
-                    <a href="/read/2">css</a>
-                </li>
-            </ol>
             <h2>Read!!</h2>
             Hello, {id}
-            <ul>
-                <li>
-                    <a href="/create">create</a>
-                </li>
-                <li>
-                    <a href="/update/id">update</a>
-                </li>
-                <li>
-                    <button>delete</button>
-                </li>
-            </ul>
         </div>
     );
 }
