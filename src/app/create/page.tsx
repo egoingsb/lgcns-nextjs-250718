c359df5c7db3ed484add1835ed9e1744592fe294
egoing <egoing@gmail.com>
2025-07-18 16:30:06 +0900

    onSubmit은 사용자와 상호작용 api이고, 자연스럽게 클라이언트로 js를 전송해야 한다. 따라서 server component를 사용할 수 없기 때문에 client component로 전환해야 한다. 그래서 "use client"를 앱의 초입에 선언했다.

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,8 +1,55 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Create() {
+    const router = useRouter();
+    // @ts-expect-error evt는 나중에 처리하겠음.
+    const submitHandler = (evt) => {
+        const target = evt.target as HTMLFormElement;
+        const title = target._title.value;
+        const body = target._body.value;
+        console.log("🚀 ~ submitHandler ~ title:", title, body);
+        fetch("http://localhost:9999/topics", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title: title,
+                body: body,
+            }),
+        })
+            .then((resp) => resp.json())
+            .then((result) => {
+                console.log(result);
+                const id = result.id;
+                router.push("/read/" + id);
+                router.refresh();
+            });
+        evt.preventDefault();
+    };
     return (
         <div>
             <h2>Create</h2>
-            Hello, Create
+            <form onSubmit={submitHandler}>
+                <p>
+                    <input
+                        type="text"
+                        name="_title"
+                        placeholder="제목을 입력하시오."
+                    />
+                </p>
+                <p>
+                    <textarea
+                        name="_body"
+                        placeholder="본문을 입력하시오."
+                    ></textarea>
+                </p>
+                <p>
+                    <input type="submit" value="생성" />
+                </p>
+            </form>
         </div>
     );
 }
